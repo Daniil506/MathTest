32c5134af66d637cd41bcc9f49e18dff252a8778
Даниил <rebrovdanil506@gmail.com>
2026-09-17 22:22:40 +0300

    исправлено деление

--- GoogleTest/TaskTests.cpp
+++ GoogleTest/TaskTests.cpp
@@ -11,6 +11,17 @@ TEST(TaskTest, DefaultConstructorInRange) {
     }
 }
 
+TEST(TaskTest, DefaultConstructorDivisionExact) {
+    for (int i = 0; i < 200; ++i) {
+        Task t;
+        if (t.operation == '/') {
+            EXPECT_NE(t.num_2, 0);
+            EXPECT_EQ(t.num_1 % t.num_2, 0);
+            EXPECT_LE(t.num_2, t.num_1);
+        }
+    }
+}
+
 TEST(TaskTest, InitConstructorRange) {
     for (int i = 0; i < 100; ++i) {
         Task t(5, 10);
@@ -29,9 +40,57 @@ TEST(TaskTest, InitConstructorFixedOperation) {
     }
 }
 
+TEST(TaskTest, InitConstructorSubtraction) {
+    for (int i = 0; i < 100; ++i) {
+        Task t(1, 10, '-');
+        EXPECT_EQ(t.operation, '-');
+        EXPECT_EQ(t.answer, t.num_1 - t.num_2);
+    }
+}
+
+TEST(TaskTest, InitConstructorMultiplication) {
+    for (int i = 0; i < 100; ++i) {
+        Task t(1, 10, '*');
+        EXPECT_EQ(t.operation, '*');
+        EXPECT_EQ(t.answer, t.num_1 * t.num_2);
+    }
+}
+
+TEST(TaskTest, DivisionAlwaysExact) {
+    for (int i = 0; i < 200; ++i) {
+        Task t(1, 20, '/');
+        EXPECT_NE(t.num_2, 0);
+        EXPECT_EQ(t.num_1 % t.num_2, 0);
+    }
+}
+
+TEST(TaskTest, DivisionDivisorLessOrEqualDividend) {
+    for (int i = 0; i < 200; ++i) {
+        Task t(5, 50, '/');
+        EXPECT_NE(t.num_2, 0);
+        EXPECT_LE(t.num_2, t.num_1);
+    }
+}
+
+TEST(TaskTest, DivisionExactAndLess) {
+    for (int i = 0; i < 200; ++i) {
+        Task t(2, 30, '/');
+        EXPECT_NE(t.num_2, 0);
+        EXPECT_EQ(t.num_1 % t.num_2, 0);
+        EXPECT_LE(t.num_2, t.num_1);
+    }
+}
+
 TEST(TaskTest, DivisionNoZeroDivisor) {
     for (int i = 0; i < 200; ++i) {
         Task t(0, 5, '/');
         EXPECT_NE(t.num_2, 0);
     }
+}
+
+TEST(TaskTest, ToStringFormat) {
+    Task t(1, 5, '+');
+    std::string s = t.to_string();
+    EXPECT_NE(s.find(' '), std::string::npos);
+    EXPECT_NE(s.find('+'), std::string::npos);
 }
--- Math.lib/Task.cpp
+++ Math.lib/Task.cpp
@@ -2,8 +2,10 @@
 #include <cstdlib>
 #include <stdexcept>
 #include <utility>
+#include <algorithm>
 
 static int rand_range(int min, int max) {
+    if (min > max) std::swap(min, max);
     return min + std::rand() % (max - min + 1);
 }
 
@@ -22,21 +24,51 @@ static int compute(int a, int b, char op) {
     }
 }
 
+static void generate_division(int min, int max, int& num_1, int& num_2) {
+    if (min < 1) min = 1;
+    if (max < min) max = min;
+
+    num_2 = rand_range(min, max);
+    if (num_2 == 0) num_2 = 1;
+
+    int k_min = (min + num_2 - 1) / num_2;
+    int k_max = max / num_2;
+    if (k_min > k_max) {
+        k_min = (min + num_2 - 1) / num_2;
+        k_max = k_min;
+    }
+    int k = rand_range(k_min, k_max);
+    num_1 = num_2 * k;
+
+    if (num_2 > num_1) std::swap(num_1, num_2);
+}
+
 Task::Task() {
-    num_1 = rand_range(1, 20);
-    num_2 = rand_range(1, 20);
     operation = rand_operation();
-    if (operation == '/' && num_2 == 0) num_2 = 1;
+
+    if (operation == '/') {
+        generate_division(1, 20, num_1, num_2);
+    }
+    else {
+        num_1 = rand_range(1, 20);
+        num_2 = rand_range(1, 20);
+    }
+
     answer = compute(num_1, num_2, operation);
 }
 
 Task::Task(int min, int max, char operation) {
     if (min > max) std::swap(min, max);
-
-    num_1 = rand_range(min, max);
-    num_2 = rand_range(min, max);
     this->operation = (operation == '\0') ? rand_operation() : operation;
-    if (this->operation == '/' && num_2 == 0) num_2 = 1;
+
+    if (this->operation == '/') {
+        generate_division(min, max, num_1, num_2);
+    }
+    else {
+        num_1 = rand_range(min, max);
+        num_2 = rand_range(min, max);
+    }
+
     answer = compute(num_1, num_2, this->operation);
 }
 
