a8327748a50916f84fcb0e39b6856c4761659864
Даниил <rebrovdanil506@gmail.com>
2026-09-17 10:32:37 +0300

    исправил ошибки ревьюера

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     std::srand(static_cast<unsigned>(std::time(nullptr)));
 
-    MathTest test(5, 1, 20);   // 5 §Ó§à§á§â§à§ã§à§Ó, §é§Ú§ã§Ý§Ñ §à§ä 1 §Õ§à 20
+    MathTest test(5, 1, 20);   
     test.run();
 
     return 0;
